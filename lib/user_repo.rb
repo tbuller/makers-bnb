59e2f997231617f5b911ab6a2a33de380b218dcd
Alastair <alastair.chau@gmail.com>
2022-11-08 12:05:30 +0000

    user repo spec tests done

--- lib/user_repo.rb
+++ lib/user_repo.rb
@@ -27,11 +27,11 @@ class UserRepository
     result_set = DatabaseConnection.exec_params(sql, [id])
 
     user = User.new
-    user.id = record['id'].to_i
-    user.name = record['name']
-    user.username = record['username']
-    user.email = record['email']
-    user.password = record['password']
+    user.id = result_set[0]['id'].to_i
+    user.name = result_set[0]['name']
+    user.username = result_set[0]['username']
+    user.email = result_set[0]['email']
+    user.password = result_set[0]['password']
 
     return user
   end
--- spec/user_repo_spec.rb
+++ spec/user_repo_spec.rb
@@ -21,4 +21,42 @@ describe UserRepository do
     expect(users.first.id).to eq(1)
     expect(users.last.name).to eq('The Rock')
   end
+
+  it "finds a single user" do
+    repo = UserRepository.new
+    user = repo.find(2)
+    expect(user.id).to eq(2)
+    expect(user.name).to eq('Stone Cold Steve Austin')
+    expect(user.username).to eq('stonecoldstunner')
+  end
+
+  it "finds a single user" do
+    repo = UserRepository.new
+    user = repo.find(3)
+    expect(user.id).to eq(3)
+    expect(user.name).to eq('The Rock')
+    expect(user.username).to eq('peopleseyebrow')
+  end
+
+  it "creates a new user" do
+    repo = UserRepository.new
+
+    new_user = User.new
+    new_user.name = 'Mankind'
+    new_user.username = 'mrsocko'
+    new_user.email = 'mandibleclaw@weemail.com'
+    new_user.password = 'Password'
+
+    repo.create(new_user)
+
+    users = repo.all
+
+    expect(users.length).to eq(4)
+    expect(users.last.id).to eq(4)
+    expect(users.last.name).to eq('Mankind')
+    expect(users.last.username).to eq('mrsocko')
+    expect(users.last.email).to eq('mandibleclaw@weemail.com')
+    expect(users.last.password).to eq('Password')
+  end
+
 end
