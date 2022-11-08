27b87243222351ecb25c4fe6f90f9ca774e043ad
Tim Buller <111061411+tbuller@users.noreply.github.com>
2022-11-08 17:15:39 +0000

    login push

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'sinatra/base'
 require 'sinatra/reloader'
 require_relative './lib/listing_repo'
 require_relative './lib/database_connection'
+require_relative './lib/user_repo'
 
 DatabaseConnection.connect
 
@@ -17,4 +18,22 @@ class Application < Sinatra::Base
 
     return erb(:index)
   end
+
+  get '/login' do
+    return erb(:login)
+  end 
+  
+  post '/login' do
+    repo = UserRepository.new
+    
+    user = repo.find_by_email(params[:email])
+    user_password = user.password
+    p user.password
+    if repo.valid_password?(user_password, params[:password]) == "success"
+      redirect '/'
+    else  
+      redirect '/login'
+    end    
+  
+  end  
 end
--- spec/integration/app_spec.rb
+++ spec/integration/app_spec.rb
@@ -5,7 +5,6 @@ require "rack/test"
 require_relative '../../app'
 require 'json'
 
-
 describe Application do
   
   include Rack::Test::Methods
@@ -25,4 +24,23 @@ describe Application do
       expect(response.body).to include('$760.00')
     end
   end
+
+  context 'GET /login' do
+    it 'logs the user in' do
+      response = get('/login')
+      
+      expect(response.status). to eq 200
+      expect(response.body).to include 'Login'
+      
+      
+    end
+  end 
+  
+  context 'POST /login' do
+    it 'Creates a form for login' do
+
+      response = post('/login', email: 'champ@weemail.com', password: 'heatbreakhotel')
+      expect(response.status).to eq 302
+    end
+  end    
 end
