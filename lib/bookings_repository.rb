fcfd1ff3ccf195800311709aa0cb7101ac11414e
Tom Fyfe <tomfyfe@MASTUDENT-Tom-Fyfe-W7JGKXKG9H.local>
2022-11-08 13:08:19 +0000

    find and create tests pass

--- lib/bookings_repository.rb
+++ lib/bookings_repository.rb
@@ -18,26 +18,26 @@ class BookingRepository
     return bookings
   end 
 
-#   def find(id)
-#     sql = 'SELECT id, date, user_id, listing_id FROM bookings WHERE id = $1;'
-#     params = [id]
-#     result_set = DatabaseConnection.exec_params(sql, params)
+  def find(id)
+    sql = 'SELECT id, date, user_id, listing_id FROM bookings WHERE id = $1;'
+    params = [id]
+    result_set = DatabaseConnection.exec_params(sql, params)
 
-#     result_set.each do |record|
-#       booking = Booking.new
-#       booking.id = record['id'].to_i
-#       booking.date = record['date']
-#       booking.user_id = record['user_id']
-#       booking.listing_id = record['listing_id']
+    result_set.each do |record|
+      booking = Booking.new
+      booking.id = record['id'].to_i
+      booking.date = record['date']
+      booking.user_id = record['user_id'].to_i
+      booking.listing_id = record['listing_id'].to_i
 
-#       return booking
-#     end 
-#   end 
+      return booking
+    end 
+  end 
   
-#   def create(booking)
-#     sql = 'INSERT INTO bookings (date, user_id, listing_id) VALUES($1, $2, $3);'
-#     sql_params = [booking.date, booking.user_id, booking.listing_id]
-#     DatabaseConnection.exec_params(sql, sql_params)
-#     return nil
-#   end 
+  def create(booking)
+    sql = 'INSERT INTO bookings (date, user_id, listing_id) VALUES($1, $2, $3);'
+    sql_params = [booking.date, booking.user_id, booking.listing_id]
+    DatabaseConnection.exec_params(sql, sql_params)
+    return nil
+  end 
 end 
--- spec/bookings_repo_spec.rb
+++ spec/bookings_repo_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../lib/bookings_repository'
+require_relative '../lib/database_connection'
  
 def reset_bookings_table
   seed_sql = File.read('spec/seeds.sql')
@@ -14,6 +15,7 @@ RSpec.describe BookingRepository do
 
   it 'returns the first of the list of bookings' do 
     repo = BookingRepository.new 
+    
     booking = repo.all
 
     expect(booking.length).to eq(6)
@@ -21,5 +23,30 @@ RSpec.describe BookingRepository do
     expect(booking.first.date).to eq ('2022-05-01')
     expect(booking.first.user_id).to eq(1)
     expect(booking.first.listing_id).to eq(1)
+  end 
+
+  it 'returns a single booking and info from id = "1"' do 
+    repo = BookingRepository.new
+    booking = repo.find(1)
+    expect(booking.date).to eq('2022-05-01')
+    expect(booking.user_id).to eq(1)
+    expect(booking.listing_id).to eq(1)
+   end 
+
+   it 'creates a new booking' do 
+    repo = BookingRepository.new 
+
+    booking = Booking.new 
+    booking.date = '2022-06-06'
+    booking.user_id = 2
+    booking.listing_id = 6
+
+    repo.create(booking)
+    
+    bookings = repo.all
+
+    last_booking = bookings.last
+    expect(last_booking.date).to eq '2022-06-06'
+    expect(last_booking.listing_id).to eq 6
   end 
 end
