1b59bce3402998ecca4065358b21cfe521221fa3
adonl <adnlwly@gmail.com>
2022-11-09 12:37:15 +0000

    update booking model with approved accessor

--- lib/bookings.rb
+++ lib/bookings.rb
@@ -1,3 +1,7 @@
 class Booking
-  attr_accessor :id, :date, :user_id, :listing_id
+  attr_accessor :id, :date, :user_id, :listing_id, :approved
+
+  def initialize
+    @approved = false
+  end
 end 
