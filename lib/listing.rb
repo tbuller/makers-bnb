95c7171558a5139b2ab206efc86d69cccd947076
Tim Buller <111061411+tbuller@users.noreply.github.com>
2022-11-10 16:43:01 +0100

    Update listing.rb

--- lib/listing.rb
+++ lib/listing.rb
@@ -2,7 +2,7 @@ class Listing
   attr_accessor :id, :name, :address, :city, :country, :ppn, :description, :host_id, :available_start, :available_end, :bookings
 
   def initialize 
-    @dates = []
+    @bookings = []
   end 
 
   
